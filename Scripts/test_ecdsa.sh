0d54e1f6afe07b019db9a3886a00c5a3be8e85c2
Marcel Keller <mks.keller@gmail.com>
2023-09-20 21:15:08 +1000

    Fix bugs in ECDSA test.

--- Scripts/test_ecdsa.sh
+++ Scripts/test_ecdsa.sh
@@ -3,14 +3,14 @@
 echo SECURE = -DINSECURE >> CONFIG.mine
 touch ECDSA/Fake-ECDSA.cpp
 
-make -j4 ecdsa Fake-ECDSA.x secure.x
+make -j4 ecdsa Fake-ECDSA.x
 
 run()
 {
     port=$[RANDOM+1024]
     if ! {
 	    for j in $(seq 0 $2); do
-		./$1-ecdsa-party.x -p $j 1 2>/dev/null & true
+		./$1-ecdsa-party.x -pn $port -p $j 1 2>/dev/null & true
 	    done
 	    wait
 	} | grep "Online checking"; then
@@ -26,7 +26,6 @@ for i in semi mascot; do
     run $i 1
 done
 
-if ! ./secure.x; then
-    ./Fake-ECDSA.x
-    run fake-spdz 1
-fi
+
+./Fake-ECDSA.x
+run fake-spdz 1
